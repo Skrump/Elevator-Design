f829fad2d75f81ffc2be5e15659987727fa4fab5
skrumpcious <iamfes007@aol.com>
2017-10-13 21:25:50 +0000

    Updated main

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/****************************************************************
+ * Authors: Alejandro Lopez, Pierce Tu, Dane Godard, Kenneth Paw
+ * Elevator Design Project
+ * Course Project for Software Engineering 1 (CMPE 131)
+ * Professor: Dr. Hungwen Li
+ * Last Modified: 10/13/2017
+ * Description: This program will improve upon basic elevator 
+ * programs. It utilizes a priority queue to process the elevator
+ * requests per floor.
+ ***************************************************************/
+ 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
