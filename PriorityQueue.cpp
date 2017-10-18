97cf2cf99b07642ab78a2bc92f4c85038a6dd2d2
skrumpcious <iamfes007@aol.com>
2017-10-18 05:50:06 +0000

    Added private & public variables/functions in PriorityQueue header -- Also added function skeletons in source file

--- PriorityQueue.cpp
+++ PriorityQueue.cpp
@@ -1,1 +1,48 @@
 #include "PriorityQueue.h"
+
+PriorityQueue::PriorityQueue()
+{
+    root = new node;
+    root->next = 0;
+    root->x = 5;
+}
+
+PriorityQueue::~PriorityQueue()
+{
+    delete root;
+}
+
+void PriorityQueue::linkedList()
+{
+    
+}
+
+bool PriorityQueue::empty()
+{
+    
+}
+
+int PriorityQueue::size()
+{
+    
+}
+
+node PriorityQueue::front()
+{
+    
+}
+
+void PriorityQueue::enqueue()
+{
+    
+}
+
+void PriorityQueue::dequeue()
+{
+    
+}
+
+void PriorityQueue::deleteEnd()
+{
+    
+}
--- PriorityQueue.h
+++ PriorityQueue.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "node.h"
+
+class PriorityQueue
+{
+        node *root;
+        void linkedList();
+    public:
+        PriorityQueue();
+        ~PriorityQueue();
+        bool empty();
+        int size();
+        node front();
+        void enqueue();
+        void dequeue();
+        void deleteEnd();
+};
