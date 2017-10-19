520012fd9b8bafa503a8f9c2ace7716c85de819b
piercetu <piercetu@gmail.com>
2017-10-18 21:02:39 -0700

    Update Elevator.cpp

--- Elevator.cpp
+++ Elevator.cpp
@@ -1,1 +1,14 @@
 #include "Elevator.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  double currWeight;
+  double maxWeight;
+  int mostReqFloor;
+  bool status;
+  int numFloors;
+
+  return 0;
+}
