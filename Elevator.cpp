07e5aaa25862a78441fc84a99cf09ad035c091b5
KenPaw <32811492+KenPaw@users.noreply.github.com>
2017-10-19 00:17:30 -0700

    Defined Elevator class and functions
    Added private variable string name and relating functions

--- Elevator.cpp
+++ Elevator.cpp
@@ -1,1 +1,104 @@
 #include "Elevator.h"
+#include "Floor.h"
+
+Elevator::Elevator()
+{
+	string = "Elevator 0";
+	currWeight = 0;
+	maxWeight = 0;
+	mostReqFloor = 0;
+	status = true;
+	numFloors();
+}
+
+Elevator::Elevator(string a, double max, double curr, int most, bool stat)
+{
+	string = a;
+	currWeight = curr;
+	maxWeight = max;
+	mostReqFloor = most;
+	status = stat;
+	numFloors();
+}
+
+string Elevator::getName()
+{
+	return name;
+}
+
+void Elevator::changeName(string a)
+{
+	name = a;
+}
+
+double Elevator::getCurrWeight()
+{
+	return currWeight;
+}
+
+void Elevator::addCurrWeight(double num)
+{
+	currWeight = num;
+}
+
+double Elevator::getMaxWeight()
+{
+	return maxWeight;
+}
+
+void Elevator::addMaxWeight(double num)
+{
+	maxWeight = num;
+}
+
+int Elevator::getMostReqFloor()
+{
+	return mostReqFloor;
+}
+
+void Elevator::addMostReqFloor(int num)
+{
+	mostReqFloor = num;
+}
+
+void Elevator::printDisplay()
+{
+	cout << "display" << endl; //wip
+}
+
+bool Elevator::goUp()
+{
+	//wip
+}
+
+bool Elevator::goDown()
+{
+	//wip
+}
+
+bool Elevator::openDoors()
+{
+	//wip
+}
+
+bool Elevator::closeDoors()
+{
+	//wip
+}
+
+void Elevator::updateStatus()
+{
+	//wip
+}
+
+int Elevator::emergencyState()
+{
+	//wip
+	return 0;
+}
+
+void Elevator::grantPriority()
+{
+	//wip
+	cout << name << " has priority" << endl;
+}
--- Elevator.h
+++ Elevator.h
@@ -1,2 +1,36 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+#include "Floor.h"
+using namespace std;
+
+class Elevator
+{
+private:
+	string name;
+	double currWeight;
+	double maxWeight;
+	int mostReqFloor;
+	bool status;
+	Floor numFloors; //wip
+public:
+	Elevator();
+	Elevator(string a, double max, double curr, int most, bool stat);
+	string getName();
+	void changeName(string a);
+	double getCurrWeight();
+	void addCurrWeight(double num);
+	double getMaxWeight();
+	void addMaxWeight(double num);
+	int getMostReqFloor();
+	void addMostReqFloor(int num);
+	void printDisplay();
+	bool goUp();
+	bool goDown();
+	bool openDoors();
+	bool closeDoors();
+	void updateStatus();
+	int emergencyState();
+	void grantPriority();
+};
