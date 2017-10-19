8e614462f7aa14f54dda832f3652858d77de23a1
piercetu <piercetu@gmail.com>
2017-10-18 21:08:09 -0700

    Update Elevator.h

--- Elevator.h
+++ Elevator.h
@@ -1,21 +1,22 @@
 #pragma once
-#incldue <iostream>
 
-using namespace std;
-
-void addFloor();
-void addMax();
-void addWeight(int w);
-double getWeight();
-void printDisplay();
-bool goDown();
-bool goUp();
-bool openDoors();
-bool closeDoors();
-void updateStatus();
-void addMost();
-int getMost();
-bool updateCurr();
-int getCurr();
-int emergencyState();
-void grantPriority();
+class Elevator(double maxWeight, double currWeight, int mostReqFloor, bool status)
+{
+  public: 
+    void addFloor();
+    void addMax();
+    void addWeight(int w);
+    double getWeight();
+    void printDisplay();
+    bool goDown();
+    bool goUp();
+    bool openDoors();
+    bool closeDoors();
+    void updateStatus();
+    void addMost();
+    int getMost();
+    bool updateCurr();
+    int getCurr();
+    int emergencyState();
+    void grantPriority();
+};
