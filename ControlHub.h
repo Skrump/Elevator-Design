272481bdb05ad0e019f7cda53f23bbaaba0178f3
dgodard1995 <dane.godard@sjsu.edu>
2017-11-28 13:07:45 -0800

    Deleted comments for the final product

--- ControlHub.h
+++ ControlHub.h
@@ -20,33 +20,33 @@
 
 using namespace std;
 
-class ControlHub { //this WIP class is for complex algorithms involving the floor priority & determining which elevator moves upon a request, etc.
+class ControlHub {
 
-	int numFloors;	//# of floors in the building
-	int elevators; //# of elevators in the system
-	bool *locked; //this array is for determining which elevators are locked to the public (*locked[])
-	PriorityQueue myQueue;//Queue Object
-	vector<Elevator> myElevator;	//Elevator Object (must change to an array at a later point)
+	int numFloors;
+	int elevators;
+	bool *locked;
+	PriorityQueue myQueue;
+	vector<Elevator> myElevator;
 	Floor *myFloorArr;
-	vector<int> topFloors; //most requested floors (left -> right is greatest -> least)
-	vector<int> inputs;	//user's inputs (gets sorted and pushed to queue)
-	vector<int> fileVec;	//used to get input from files
-	void sortVec();	//sorts the inputs vector
+	vector<int> topFloors;
+	vector<int> inputs;
+	vector<int> fileVec;
+	void sortVec();
 	void sortVec(vector<int>& vec);
-	void getFromFile();	//gets data from file
-	void writeToFile(int num);	//writes to/updates file
+	void getFromFile();
+	void writeToFile(int num);
 	void swapVal(int f, int s);
 	void updateVectors(vector<int> &v1, vector<int> &v2, vector<int> &v3, vector<int> &v4);
 
-public: //includes some functions moved from Elevator class so far, help expand if necessary
+public:
 
 	ControlHub();
 	ControlHub(int a);
-	ControlHub(int a, int f);	//constructor that takes the number of floors as well
+	ControlHub(int a, int f);
 	ControlHub(int a, int f, vector<Elevator> ele);
 	~ControlHub();
-	void runMe();	//runs the program
-	void findMostReqFloor();	//gets priority status
+	void runMe();
+	void findMostReqFloor();
 	void addMostReqFloor(int num);
 	bool getStatus(int b);
 	void printDisplay();
